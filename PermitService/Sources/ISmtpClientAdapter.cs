665ab4db843ff7cca281a938e854b59d73b4137c
Konrad-Lb <konradlb@gmail.com>
2024-12-20 14:55:12 +0100

    Added missing EmailNotification files. Created SmptClientAdapter

---
+++ PermitService/Sources/ISmtpClientAdapter.cs
@@ -0,0 +1,16 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+namespace PermitService.Sources
+{
+    public interface ISmtpClientAdapter
+    {
+        ICredentialsByHost? Credentials { get; set; }
+        SmtpDeliveryMethod DeliveryMethod { get; set; }
+        bool EnableSsl { get; set; }
+        bool UseDefaultCredentials { get; set; }
+
+        void Send(MailMessage message);
+        Task SendAsync(MailMessage message);
+    }
+}
---
+++ PermitService/Sources/SmtpClientAdapter.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PermitService.Sources
+{
+    public class SmtpClientAdapter : ISmtpClientAdapter
+    {
+        private readonly SmtpClient _smtpClient;
+
+        public SmtpClientAdapter(string? host, int port)
+        {
+            _smtpClient = new SmtpClient(host, port);
+        }
+
+        public SmtpDeliveryMethod DeliveryMethod
+        {
+            get => _smtpClient.DeliveryMethod;
+            set => _smtpClient.DeliveryMethod = value;
+        }
+
+        public bool EnableSsl
+        {
+            get => _smtpClient.EnableSsl;
+            set => _smtpClient.EnableSsl = value;
+        }
+
+        public bool UseDefaultCredentials
+        {
+            get => _smtpClient.UseDefaultCredentials;
+            set => _smtpClient.UseDefaultCredentials = value;
+        }
+
+        public ICredentialsByHost? Credentials
+        {
+            get => _smtpClient.Credentials;
+            set => _smtpClient.Credentials = value;
+        }
+
+        public void Send(MailMessage message)
+        {
+            _smtpClient.Send(message);
+        }
+
+        public async Task SendAsync(MailMessage message)
+        {
+            await _smtpClient.SendMailAsync(message);
+        }
+    }
+}
