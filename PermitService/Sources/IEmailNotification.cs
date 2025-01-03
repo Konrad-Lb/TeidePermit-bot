fc34b0a34fe78d8eb1cb5009014f8536de703357
Konrad-Lb <konradlb@gmail.com>
2025-01-03 09:16:59 +0100

    Extracted SendEmailAsync method to interface

--- PermitService/Sources/IEmailNotification.cs
+++ PermitService/Sources/IEmailNotification.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,6 @@ namespace PermitService.Sources
 {
     public interface IEmailNotification
     {
+        Task SendEmailAsync(string emailSubject, string emailBody, MailAddress recipientEmailAddress);
     }
 }
