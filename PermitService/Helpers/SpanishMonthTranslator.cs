22ae9509495ab238526c612a9d9f7252d11a0a67
Konrad-Lb <konradlb@gmail.com>
2025-01-08 12:06:58 +0100

    Implemented month enum and month Spanish names

---
+++ PermitService/Helpers/SpanishMonthTranslator.cs
@@ -0,0 +1,31 @@
+﻿
+namespace PermitService.Helpers
+{
+    public static class SpanishMonthTranslator
+    {
+        public static string GetTranslation(Month month)
+        {
+            return month switch
+            {
+                Month.January => "Enero",
+                Month.February => "Febrero",
+                Month.March => "Marzo",
+                Month.April => "Abril",
+                Month.May => "Mayo",
+                Month.June => "Junio",
+                Month.July => "Julio",
+                Month.August => "Agosto",
+                Month.September => "Septiembre",
+                Month.October => "Octubre",
+                Month.November => "Noviembre",
+                Month.December => "Diciembre",
+                _ => "Invalid month"
+            };
+        }
+
+        public static string GetTranslationLowerCase(Month month)
+        {
+            return GetTranslation(month).ToLower();
+        }
+    }
+}
