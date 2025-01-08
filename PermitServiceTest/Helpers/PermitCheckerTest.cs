58f02a758afc3cc0452e372fc9537e9ad3a747d1
Konrad-Lb <konradlb@gmail.com>
2025-01-08 12:08:19 +0100

    Added classes responsible for checking which permits are available

---
+++ PermitServiceTest/Helpers/PermitCheckerTest.cs
@@ -0,0 +1,82 @@
+﻿using Moq;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using PermitService.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PermitServiceTest.Helpers
+{
+    [TestFixture]
+    public class PermitCheckerTest
+    {
+        private Mock<IWebDriver> _webDriverMock = null!;
+        
+        [SetUp]
+        public void TestSetUp()
+        {
+            _webDriverMock = new Mock<IWebDriver>();
+            var nextStepLinkStubStub = new Mock<IWebElement>();
+            _webDriverMock.Setup(x => x.FindElement(By.Id("Button1"))).Returns(nextStepLinkStubStub.Object);
+        }
+
+        [Test]
+        public void GetAvailableDays_NoAvailableDays_DictionaryIsEmpty()
+        {
+            string currentMonthCallendar = "<html>" +
+            "<table>" +
+                "<tbody>" +
+                    "<tr>" +
+                        "<td class=\"dias\" style=\"background-color:Gray;\">" +
+                            "<a href=\"\" style=\"color:Black\" title=\"5 de mayo\">5</a>" +
+                        "</td>" +
+                        "<td class=\"dias\" style=\"background-color:Black;\">" +
+                            "<a href=\"\" style=\"color:Black\" title=\"6 de mayo\">6</a>" +
+                        "</td>"+
+                    "</tr>" +
+                "</tbody>" +
+            "</table>" +
+            "</html>";
+            _webDriverMock.Setup(x => x.PageSource).Returns(currentMonthCallendar);
+
+            var availableDays = PermitChecker.GetAvailableDays(_webDriverMock.Object);
+            Assert.That(availableDays.Count, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void GetAvailableDays_OneAvailableDayOnFifthOfMay_DictonaryHasOneEntry()
+        {
+            string currentMonthCallendar = "<html>" +
+            "<table>" +
+                "<tbody>" +
+                    "<tr>" +
+                        "<td class=\"dias\" style=\"background-color:WhiteSmoke;\">" +
+                            "<a href=\"\" style=\"color:Black\" title=\"5 de mayo\">5</a>" +
+                        "</td>" +
+                    "</tr>" +
+                "</tbody>" +
+            "</table>"+
+            "</html>";
+            _webDriverMock.Setup(x => x.PageSource).Returns(currentMonthCallendar);
+            
+            var availableDays = PermitChecker.GetAvailableDays(_webDriverMock.Object);
+            
+            Assert.That(availableDays.ContainsKey(Month.May) , Is.True);
+            Assert.That(availableDays[Month.May].Count, Is.EqualTo(1));
+            Assert.That(availableDays[Month.May].First, Is.EqualTo(5));
+        }
+
+        //diffrent date single days available test
+        //style vs bgcolor attribute
+        //webelement Button1 not foound
+        //test wait.Until
+        //tes calendar script failed
+        //no available days for 12 months
+        //one day available - others are not available
+        //current month detection on website
+        //no page content
+    }
+}
---
+++ PermitServiceTest/Helpers/SpanishMonthTranslatorTest.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+using PermitService.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PermitServiceTest.Helpers
+{
+    [TestFixture]
+    public class SpanishMonthTranslatorTest
+    {
+        [Test]
+        public void GetTranslation_CheckAllTranslations_ProperTranslationReturned()
+        {
+            Assert.That(SpanishMonthTranslator.GetTranslation(Month.January), Is.EqualTo("Enero"));
+            Assert.That(SpanishMonthTranslator.GetTranslation(Month.February), Is.EqualTo("Febrero"));
+            Assert.That(SpanishMonthTranslator.GetTranslation(Month.March), Is.EqualTo("Marzo"));
+            Assert.That(SpanishMonthTranslator.GetTranslation(Month.April), Is.EqualTo("Abril"));
+            Assert.That(SpanishMonthTranslator.GetTranslation(Month.May), Is.EqualTo("Mayo"));
+            Assert.That(SpanishMonthTranslator.GetTranslation(Month.June), Is.EqualTo("Junio"));
+            Assert.That(SpanishMonthTranslator.GetTranslation(Month.July), Is.EqualTo("Julio"));
+            Assert.That(SpanishMonthTranslator.GetTranslation(Month.August), Is.EqualTo("Agosto"));
+            Assert.That(SpanishMonthTranslator.GetTranslation(Month.September), Is.EqualTo("Septiembre"));
+            Assert.That(SpanishMonthTranslator.GetTranslation(Month.October), Is.EqualTo("Octubre"));
+            Assert.That(SpanishMonthTranslator.GetTranslation(Month.November), Is.EqualTo("Noviembre"));
+            Assert.That(SpanishMonthTranslator.GetTranslation(Month.December), Is.EqualTo("Diciembre"));
+        }
+
+        [Test]
+        public void GetTranslationLowerCase()
+        {
+            Assert.That(SpanishMonthTranslator.GetTranslationLowerCase(Month.January), Is.EqualTo("enero"));
+            Assert.That(SpanishMonthTranslator.GetTranslationLowerCase(Month.February), Is.EqualTo("febrero"));
+            Assert.That(SpanishMonthTranslator.GetTranslationLowerCase(Month.March), Is.EqualTo("marzo"));
+            Assert.That(SpanishMonthTranslator.GetTranslationLowerCase(Month.April), Is.EqualTo("abril"));
+            Assert.That(SpanishMonthTranslator.GetTranslationLowerCase(Month.May), Is.EqualTo("mayo"));
+            Assert.That(SpanishMonthTranslator.GetTranslationLowerCase(Month.June), Is.EqualTo("junio"));
+            Assert.That(SpanishMonthTranslator.GetTranslationLowerCase(Month.July), Is.EqualTo("julio"));
+            Assert.That(SpanishMonthTranslator.GetTranslationLowerCase(Month.August), Is.EqualTo("agosto"));
+            Assert.That(SpanishMonthTranslator.GetTranslationLowerCase(Month.September), Is.EqualTo("septiembre"));
+            Assert.That(SpanishMonthTranslator.GetTranslationLowerCase(Month.October), Is.EqualTo("octubre"));
+            Assert.That(SpanishMonthTranslator.GetTranslationLowerCase(Month.November), Is.EqualTo("noviembre"));
+            Assert.That(SpanishMonthTranslator.GetTranslationLowerCase(Month.December), Is.EqualTo("diciembre"));
+        }
+    }
+}
