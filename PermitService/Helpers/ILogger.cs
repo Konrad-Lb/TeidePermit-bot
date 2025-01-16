8937f3ec396356d7ab15eb769ed9ebd5440ca3f2
Konrad-Lb <konradlb@gmail.com>
2025-01-16 14:33:02 +0100

    Added Logger wrapper

---
+++ PermitService/Helpers/ILogger.cs
@@ -0,0 +1,10 @@
+﻿namespace PermitService.Helpers
+{
+    public interface ILogger
+    {
+        void Debug(string logMessage);
+        void Error(string logMessage);
+        void Info(string logMessage);
+        void Warning(string logMessage);
+    }
+}
---
+++ PermitService/Helpers/Logger.cs
@@ -0,0 +1,32 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PermitService.Helpers
+{
+    public class Logger(ILog logger) : ILogger
+    {
+        public void Info(string logMessage)
+        {
+            logger.Info("[INFO] " + logMessage);
+        }
+
+        public void Error(string logMessage)
+        {
+            logger.Error("[ERROR] " + logMessage);
+        }
+
+        public void Warning(string logMessage)
+        {
+            logger.Warn("[WARNING] " + logMessage);
+        }
+
+        public void Debug(string logMessage)
+        {
+            logger.Debug("[DEBUG] " + logMessage);
+        }
+    }
+}
