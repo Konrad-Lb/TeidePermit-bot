54e8ba1f6cde05fb612f986de1892d3e7b5832b6
Konrad-Lb <konradlb@gmail.com>
2025-01-31 08:37:18 +0100

    Commit tests

---
+++ PermitServiceTest/Configuration/SenderEmailAddressTest.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using PermitService.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PermitServiceTest.Configuration
+{
+    [TestFixture]
+    public class SenderEmailAddressTest
+    {
+        [TestCase("test@test.com", "displayName", Description = "ConvertToMailAddress_DisplayNameIsNull_ObjectsAreEqual")]
+        [TestCase("test@test.com", null, Description = "ConvertToMailAddress_DisplayNameIsNotNull_ObjectsAreEqual")]
+        public void ConvertToMailAddress_ConstructorParametersAreTheSame_ObjectsAreEqual(string emailAddress, string? displayName)
+        {
+            var mailAddress = new MailAddress(emailAddress, displayName);
+            var senderEmailAddress = new SenderEmailAddress(emailAddress, displayName);
+
+            Assert.That(mailAddress.Equals((MailAddress)senderEmailAddress));
+        }
+
+        [Test]
+        public void ConvertToMailAddress_WrongEmailAddressFormat_ThrowsFormatException()
+        {
+            var mailAddress = new MailAddress("test@test.com", "displayName");
+            var senderEmailAddress = new SenderEmailAddress("aaa", "displayName");
+
+            Assert.Throws<FormatException>(() => mailAddress.Equals((MailAddress)senderEmailAddress));
+        }
+    }
+}
---
+++ PermitServiceTest/Helpers/LoggerTest.cs
@@ -0,0 +1,56 @@
+﻿using log4net;
+using log4net.Repository.Hierarchy;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PermitServiceTest.Helpers
+{
+    [TestFixture]
+    public class LoggerTest
+    {
+        [Test]
+        public void Info_LogMessage_AddedInfoPrefix()
+        {
+            var log4netMock = new Mock<ILog>();
+            var logger = new PermitService.Helpers.Log4NetAdapter(log4netMock.Object);
+            logger.Info("Info text");
+
+            log4netMock.Verify(x => x.Info("[INFO] Info text"), Times.Once);
+        }
+
+        [Test]
+        public void Info_LogMessage_AddedWarningPrefix()
+        {
+            var log4netMock = new Mock<ILog>();
+            var logger = new PermitService.Helpers.Log4NetAdapter(log4netMock.Object);
+            logger.Warning("Warning text");
+
+            log4netMock.Verify(x => x.Warn("[WARNING] Warning text"), Times.Once);
+        }
+
+        [Test]
+        public void Info_LogMessage_AddedErrorPrefix()
+        {
+            var log4netMock = new Mock<ILog>();
+            var logger = new PermitService.Helpers.Log4NetAdapter(log4netMock.Object);
+            logger.Error("Error text");
+
+            log4netMock.Verify(x => x.Error("[ERROR] Error text"), Times.Once);
+        }
+
+        [Test]
+        public void Info_LogMessage_AddedDebugPrefix()
+        {
+            var log4netMock = new Mock<ILog>();
+            var logger = new PermitService.Helpers.Log4NetAdapter(log4netMock.Object);
+            logger.Debug("Debug text");
+
+            log4netMock.Verify(x => x.Debug("[DEBUG] Debug text"), Times.Once);
+        }
+    }
+}
