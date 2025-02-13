a06d27bb1949e08fa0287b2926544d43adbdcab5
Konrad-Lb <konradlb@gmail.com>
2025-02-13 09:51:52 +0100

    Implemented sending email notification for available days

--- PermitService/Sources/NotificationSender.cs
+++ PermitService/Sources/NotificationSender.cs
@@ -1,7 +1,9 @@
-﻿using PermitService.Helpers;
+﻿using OpenQA.Selenium.BiDi.Modules.Network;
+using PermitService.Helpers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,9 +11,33 @@ namespace PermitService.Sources
 {
     public class NotificationSender(IEmailSender emailSender)
     {
-        public void SendMotification(IDictionary<Month, List<int>> availableDays, PermitRequestData requestData)
+        public async Task SendMotification(IDictionary<Month, List<int>> availableDays, PermitRequestData requestData)
         {
+            var emailBody = new StringBuilder();
+            var currentDate = requestData.StartDate;
+            while(currentDate <= requestData.EndDate)
+            {
+                emailBody.Append(GenerateEmailBodyForDate(availableDays, currentDate));
+                currentDate = currentDate.AddDays(1);
+            }
 
+           await SendEmailIfBodyNotEmpty(emailBody.ToString(), requestData.EmailAddress);
+        }
+
+        private static string GenerateEmailBodyForDate(IDictionary<Month, List<int>> availableDays, DateTime date)
+        {
+            var month = (Month)date.Month;
+
+            if (availableDays.TryGetValue(month, out var day) && day.Contains(date.Day))
+                return $"New permits are avaliable for date {date.Day} of {month}\n";
+
+            return string.Empty;
+        }
+
+        private async Task SendEmailIfBodyNotEmpty(string emailBody, string emailAddress)
+        {
+            if (!string.IsNullOrEmpty(emailBody) && !string.IsNullOrEmpty(emailAddress)) 
+                await emailSender.SendEmailAsync("New Teide permits are available", emailBody, new MailAddress(emailAddress));
         }
     }
 }
--- PermitServiceTest/Sources/NotificationSenderTest.cs
+++ PermitServiceTest/Sources/NotificationSenderTest.cs
@@ -15,25 +15,41 @@ namespace PermitServiceTest.Sources
     public class NotificationSenderTest
     {
         [Test]
-        public void SendNotification_NoAvailableDaysForRequestDataPeriod_NoEmailSent()
+        public async Task SendNotification_NoAvailableDaysForRequestDataPeriod_NoEmailSent()
         {
             var emailSenderMock = new Mock<IEmailSender>();
-            var requestData = new PermitRequestData { StartDate = new DateTime(2025, 1, 31), EndDate = new DateTime(2024, 2, 15), EmailAddress = "test@test.com"};
+            var requestData = new PermitRequestData { StartDate = new DateTime(2025, 1, 31), EndDate = new DateTime(2025, 2, 15), EmailAddress = "test@test.com"};
             var availableDays = new Dictionary<Month, List<int>> 
             {
-                { Month.January, new List<int>{1, 26, 27, 28, 29, 30 } },
-                { Month.February, new List<int>{16, 17, 18, 19, 20 } },
-                { Month.March, new List<int> {1} }
+                { Month.January, new List<int>{ 1, 26, 27, 28, 29, 30 } },
+                { Month.February, new List<int>{ 16, 17, 18, 19, 20 } },
+                { Month.March, new List<int> { 1 } }
             };
 
-            new NotificationSender(emailSenderMock.Object).SendMotification(availableDays, requestData);
+            var sender = new NotificationSender(emailSenderMock.Object);
+            await sender.SendMotification(availableDays, requestData);
 
             emailSenderMock.Verify(x => x.SendEmailAsync(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<MailAddress>()), Times.Never);
+        }
 
+        [Test]
+        public async Task SendNotification_FewDaysAvailableForRequestedDataPerriod_OneEmailSent()
+        {
+            var emailSenderMock = new Mock<IEmailSender>();
+            var requestData = new PermitRequestData { StartDate = new DateTime(2025, 1, 31), EndDate = new DateTime(2025, 2, 15), EmailAddress = "test@test.com"};
+            var availableDays = new Dictionary<Month, List<int>> 
+            {
+                { Month.February, new List<int>{ 5, 8, 12 } }
+            };
+
+            var sender = new NotificationSender(emailSenderMock.Object);
+            await sender.SendMotification(availableDays, requestData);
+
+            emailSenderMock.Verify(x => x.SendEmailAsync("New Teide permits are available",
+                "New permits are avaliable for date 5 of February\n" +
+                "New permits are avaliable for date 8 of February\n" +
+                "New permits are avaliable for date 12 of February\n", 
+                It.Is<MailAddress>(x => x.Address == "test@test.com")), Times.Once);
         }
     }
-
-    //multilple days are available => send in one mail
-    //one day availanle => send in one mail
-    //no days available => send no email
 }
