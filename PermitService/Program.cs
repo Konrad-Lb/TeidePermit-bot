5820d8ceeea5c1fdb101b7efda6c8f9848fda2bf
Konrad-Lb <konradlb@gmail.com>
2025-02-14 13:10:58 +0100

    Code refactoring

--- PermitService/Program.cs
+++ PermitService/Program.cs
@@ -4,16 +4,22 @@ using log4net.Repository;
 using PermitService;
 using PermitService.Configuration;
 using PermitService.Helpers;
+using PermitService.Sources;
 using System.Reflection;
 
 
+Directory.SetCurrentDirectory(AppDomain.CurrentDomain.BaseDirectory);
 ILoggerRepository repository = LogManager.GetRepository(Assembly.GetExecutingAssembly());
 XmlConfigurator.Configure(repository, new FileInfo("log4net.config"));
+var logger = new Log4NetAdapter(log4net.LogManager.GetLogger("LOG"));
+var fileProvider = new FileProvider();
+var csvFileManager = new CsvFileManager(logger, fileProvider, new DateTimeService(), ';');
 
 var builder = Host.CreateApplicationBuilder(args);
 builder.Services.AddHostedService<Worker>();
 builder.Services.Configure<AppSettings>(builder.Configuration.GetSection("AppSettings"));
-builder.Services.AddSingleton<ILog4NetAdapter>(new Log4NetAdapter(log4net.LogManager.GetLogger("LOG")));
+builder.Services.AddSingleton<ILog4NetAdapter>(logger);
+builder.Services.AddSingleton<CsvFileManager>(csvFileManager);
 
 var host = builder.Build();
 host.Run();
--- PermitService/Worker.cs
+++ PermitService/Worker.cs
@@ -9,12 +9,12 @@ using System.Reflection;
 using System.Net.Http.Headers;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.BiDi.Modules.Network;
 
 namespace PermitService
 {
-    public class Worker(ILog4NetAdapter logger, IOptions<AppSettings> appSettings) : BackgroundService
+    public class Worker(ILog4NetAdapter logger, CsvFileManager csvFileManager, IOptions<AppSettings> appSettings) : BackgroundService
     {
-        
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             while (!stoppingToken.IsCancellationRequested)
@@ -22,72 +22,79 @@ namespace PermitService
                 try
                 {
                     logger.Info("Start checking permits.");
-                    
-                    Directory.SetCurrentDirectory(AppDomain.CurrentDomain.BaseDirectory);
-
-                    var fileProvider = new FileProvider();
-                    var csvFileManager = new CsvFileManager(logger, fileProvider, new DateTimeService(), ';');
-                    var inputRequestData = csvFileManager.ReadInputDataAsync(@"Data\input_data.csv");
-                    var savedRequestData = csvFileManager.ReadSavedDataAsync(@"Data\saved_data.csv");
-
-                    await Task.WhenAll(inputRequestData, savedRequestData);
-
-                    var requestData = savedRequestData.Result.Concat(inputRequestData.Result);
-
-                    await csvFileManager.SavePermitRequestData(@"Data\saved_data.csv", requestData.ToList());
-
-                    var monthNumbersToCheck = requestData.Select(x => x.GetDistinctMonthNumbers()).SelectMany(x => x).Distinct().ToList();
-
-                    if (monthNumbersToCheck.Count > 0)
-                    {
-                        var webDriver = new FirefoxDriver();
-                        webDriver.Navigate().GoToUrl(appSettings.Value.ScrapURL);
-                        var webPageCrawler = new TeideWebPageClawler(webDriver);
-                        var permitChecker = new PermitChecker(webPageCrawler);
-                        var availableDays = permitChecker.GetAvailableDays(monthNumbersToCheck);
-                        webDriver.Quit();
-
-                        if (availableDays.Count > 0)
-                        {
-                            var sendTasks = new List<Task>();
-                            foreach (var singleRequest in requestData)
-                            {
-                                var smtpAdapter = new SmtpClientAdapter(appSettings.Value.SmtpClientSettings);
-                                var emailSender = new EmailSender(smtpAdapter, appSettings.Value);
-                                sendTasks.Add(HandleSendEmailExceptionAsync(NotificationSender.SendNotification(emailSender, availableDays, singleRequest), singleRequest));
-                            }
-                            await Task.WhenAll(sendTasks);
-                        }
-                        else
-                            logger.Warning("There are no available days for all requests");
-                    }
-                    else
-                        logger.Warning("There are no months to check for all requests");
+                    await CheckPermits();
                 }
                 catch (Exception ex)
                 {
                     logger.Error($"Unexpected error while checking permits {ex.Message}");
-
-                    var innerException = ex.InnerException;
-
-                    while (innerException != null)
-                    {
-                        logger.Error($"Inner exception message: {innerException.Message}");
-                        innerException = innerException.InnerException;
-                    }
+                    LogInnerExceptionMessages(ex);
                 }
 
                 logger.Info("Checking permits finished.");
                 await Task.Delay(appSettings.Value.RequestIntervalInSeconds * 1000, stoppingToken);
-
-
-                //hide firefox logs
-                //hide firefox window
-                //test invalid date time format 2025-06-5 in input data
-                //provide the same entry startdate;enddate;email in input file as there is in saved file. Duplicated entries are not removed
-                //cathc all exceptions
-                //await opimalization
             }
+        }
+
+        private async Task CheckPermits()
+        {
+            var requestData = await ReadMergeAndSavePermitRequestData();
+            var monthNumbersToCheck = requestData
+                .Select(x => x.GetDistinctMonthNumbers())
+                .SelectMany(x => x)
+                .Distinct()
+                .ToList();
+
+            if (monthNumbersToCheck.Count > 0)
+                await GetPermitAvailableDaysAndSendNotifications(monthNumbersToCheck, requestData);
+            else
+                logger.Warning("There are no months to check for all requests");
+        }
+
+        private async Task<IEnumerable<PermitRequestData>> ReadMergeAndSavePermitRequestData()
+        {
+            var inputRequestData = csvFileManager.ReadInputDataAsync(@"Data\input_data.csv");
+            var savedRequestData = csvFileManager.ReadSavedDataAsync(@"Data\saved_data.csv");
+
+            await Task.WhenAll(inputRequestData, savedRequestData);
+
+            var requestData = savedRequestData.Result.Concat(inputRequestData.Result);
+            await csvFileManager.SavePermitRequestData(@"Data\saved_data.csv", requestData.ToList());
+
+            return requestData;
+        }
+
+        private async Task GetPermitAvailableDaysAndSendNotifications(List<Month> months, IEnumerable<PermitRequestData> requestData)
+        {
+            var availableDays = GetPermitAvailableDays(months);
+
+            if (availableDays.Count > 0)
+                await NotifyUsersAboutAvailablePermits(availableDays, requestData);
+            else
+                logger.Warning("There are no available days for all requests");
+        }
+
+        private IDictionary<Month, List<int>> GetPermitAvailableDays(List<Month> months)
+        {
+            var webDriver = new FirefoxDriver();
+            webDriver.Navigate().GoToUrl(appSettings.Value.ScrapURL);
+            var webPageCrawler = new TeideWebPageClawler(webDriver);
+            var permitChecker = new PermitChecker(webPageCrawler);
+            var permitAvailableDays = permitChecker.GetAvailableDays(months);
+            webDriver.Quit();
+
+            return permitAvailableDays;
+        }
+
+        private async Task NotifyUsersAboutAvailablePermits(IDictionary<Month, List<int>> availableDays, IEnumerable<PermitRequestData> requestData)
+        {
+            var sendTasks = new List<Task>();
+            foreach (var singleRequest in requestData)
+            {
+                var smtpAdapter = new SmtpClientAdapter(appSettings.Value.SmtpClientSettings);
+                var emailSender = new EmailSender(smtpAdapter, appSettings.Value);
+                sendTasks.Add(HandleSendEmailExceptionAsync(NotificationSender.SendNotification(emailSender, availableDays, singleRequest), singleRequest));
+            }
+            await Task.WhenAll(sendTasks);
         }
 
         private async Task HandleSendEmailExceptionAsync(Task task, PermitRequestData requestData)
@@ -101,14 +108,26 @@ namespace PermitService
                 logger.Error($"Cannot send email notificiation for request permit: StartDate = {requestData.StartDate}, EndDate= {requestData.EndDate}, EmailAddress = {requestData.EmailAddress}");
                 logger.Error($"Exception message: {ex.Message}");
 
-                var innerException = ex.InnerException;
-
-                while (innerException != null)
-                {
-                    logger.Error($"Inner exception message: {innerException.Message}");
-                    innerException = innerException.InnerException;
-                }
+                LogInnerExceptionMessages(ex);
             }
         }
+
+        private void LogInnerExceptionMessages(Exception ex)
+        {
+            var innerException = ex.InnerException;
+
+            while (innerException != null)
+            {
+                logger.Error($"Inner exception message: {innerException.Message}");
+                innerException = innerException.InnerException;
+            }
+        }
+
+        //hide firefox logs
+        //hide firefox window
+        //test invalid date time format 2025-06-5 in input data
+        //provide the same entry startdate;enddate;email in input file as there is in saved file. Duplicated entries are not removed
+        //cathc all exceptions
+        //await opimalization
     }
 }
